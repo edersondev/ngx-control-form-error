80235b2a4245f86ef9d6798a7e3832008ce54c81
Ederson Ferreira <ederson.dev@gmail.com>
2020-02-02 00:41:23 -0300

    atualização do readme

--- projects/ngx-control-form-error/src/lib/form-msg-error.ts
+++ projects/ngx-control-form-error/src/lib/form-msg-error.ts
@@ -8,8 +8,7 @@ export const defaultErrors = {
   matDatepickerParse: (matDatepickerParse) => (matDatepickerParse.text == 0 ? 'Informe uma data válida' : `O valor ${matDatepickerParse.text} é inválido`),
   maxlength: ({ requiredLength, actualLength }) => `A quantidade máxima de caracteres é ${requiredLength} mas o campo possui ${actualLength}`,
   minlength: ({ requiredLength, actualLength }) => `A quantidade mínima de caracteres é ${requiredLength} mas o campo possui ${actualLength}`,
-  pattern: (requiredPattern, actualValue) => `O formato do valor ${actualValue} é inválido`,
-  mustMatch: (error) => `Os valores não conferem`
+  pattern: (requiredPattern, actualValue) => `O formato do valor ${actualValue} é inválido`
 }
 
 export const FORM_MSG_ERRORS = new InjectionToken('FORM_MSG_ERRORS', {
