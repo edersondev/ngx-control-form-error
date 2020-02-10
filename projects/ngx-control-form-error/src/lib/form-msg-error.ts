becfcb91c360de94ad38d4de9f000012ad9c36a6
Ederson Ferreira <ederson.dev@gmail.com>
2020-02-09 23:35:36 -0300

    ajuste na mensagem de erro

--- projects/ngx-control-form-error/src/lib/form-msg-error.ts
+++ projects/ngx-control-form-error/src/lib/form-msg-error.ts
@@ -1,14 +1,14 @@
 import { InjectionToken } from '@angular/core';
 
 export const defaultErrors = {
-  min: (min, actual) => `O valor mínimo é ${min} e o valor informado é ${actual}`,
+  min: (objMin) => `O valor mínimo é ${objMin.min} e o valor informado é ${objMin.actual}`,
   max: (objMax) => `O valor máximo é ${objMax.max} e o valor informado é ${objMax.actual}`,
   required: (error) => `Este campo é obrigatório`,
   emai: (email) => `Informe um e-mail válido`,
   matDatepickerParse: (matDatepickerParse) => (matDatepickerParse.text == 0 ? 'Informe uma data válida' : `O valor ${matDatepickerParse.text} é inválido`),
   maxlength: ({ requiredLength, actualLength }) => `A quantidade máxima de caracteres é ${requiredLength} mas o campo possui ${actualLength}`,
   minlength: ({ requiredLength, actualLength }) => `A quantidade mínima de caracteres é ${requiredLength} mas o campo possui ${actualLength}`,
-  pattern: (requiredPattern, actualValue) => `O formato do valor ${actualValue} é inválido`
+  pattern: (requiredPattern) => `O formato do valor ${requiredPattern} é inválido`
 }
 
 export const FORM_MSG_ERRORS = new InjectionToken('FORM_MSG_ERRORS', {
