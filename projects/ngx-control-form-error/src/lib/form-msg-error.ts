6322b2100042d631f7c212d4b67bf23cee87c583
Ederson Ferreira da Silva <ederson.silva@inep.gov.br>
2020-06-24 17:26:16 -0300

    adicionado novas validações

--- projects/ngx-control-form-error/src/lib/form-msg-error.ts
+++ projects/ngx-control-form-error/src/lib/form-msg-error.ts
@@ -4,11 +4,15 @@ export const defaultErrors = {
   min: (objMin) => `O valor mínimo é ${objMin.min} e o valor informado é ${objMin.actual}`,
   max: (objMax) => `O valor máximo é ${objMax.max} e o valor informado é ${objMax.actual}`,
   required: (error) => `Este campo é obrigatório`,
-  emai: (email) => `Informe um e-mail válido`,
+  email: (email) => `Informe um e-mail válido`,
   matDatepickerParse: (matDatepickerParse) => (matDatepickerParse.text == 0 ? 'Informe uma data válida' : `O valor ${matDatepickerParse.text} é inválido`),
   maxlength: ({ requiredLength, actualLength }) => `A quantidade máxima de caracteres é ${requiredLength} mas o campo possui ${actualLength}`,
   minlength: ({ requiredLength, actualLength }) => `A quantidade mínima de caracteres é ${requiredLength} mas o campo possui ${actualLength}`,
-  pattern: (objPattern) => `O valor '${objPattern.actualValue}' não é compatível com o formato ${objPattern.requiredPattern}`
+  pattern: (objPattern) => `O valor '${objPattern.actualValue}' não é compatível com o formato ${objPattern.requiredPattern}`,
+  mustMatch: (error) => `Os valores não conferem`,
+  fileSize: (objFilesize) => `O tamanho máximo permitido por arquivo é de ${objFilesize.limitSize}, o arquivo atual possui ${objFilesize.sizeFile}`,
+  requiredFileType: (obj) => `Tipo de arquivo inválido, este campo aceita arquivos do tipo: ${obj.types}`,
+  notMatch: (obj) => `Os valores não devem ser iguais`
 }
 
 export const FORM_MSG_ERRORS = new InjectionToken('FORM_MSG_ERRORS', {
